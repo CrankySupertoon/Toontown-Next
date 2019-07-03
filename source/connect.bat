37738709467db861f6eaf6f4bf27065c73de533b
CrankySupertoon <48419040+CrankySupertoon@users.noreply.github.com>
2019-07-03 17:47:08 -0400

    New connect script for connecting with a username

--- source/connect.bat
+++ source/connect.bat
@@ -1,2 +1,10 @@
+@echo off
+set /P ttrUsername="Username: " || ^
+set ttrUsername=username
+set TTN_PLAYCOOKIE=%ttrUsername%
+set username=%ttrUsername%
+set password=%ttrpassword%
+set TTR_GAMESERVER=127.0.0.1
+
 "dependencies\panda\python\ppython.exe" -m toontown.toonbase.ToontownStart
 pause
